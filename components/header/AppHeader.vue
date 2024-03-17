209b76badd27e7d8937c1af0accb75ae14f6c054
Andy Terry <andyterrydesign@gmail.com>
2024-03-17 20:59:07 +0000

    Update AppHeader.vue

--- components/header/AppHeader.vue
+++ components/header/AppHeader.vue
@@ -13,21 +13,7 @@
 import { ref } from "vue";
 import Button from "~/components/ui/Button.vue";
 import Intro from "~/components/sections/Intro.vue";
-import IntroElements from "~/components/elements/IntroElements.vue";
+import IntroElements from "../elements/introElements.vue"; // Updated import path
 </script>
 
 
-
-<style scoped>
-.intro-text {
-  @apply relative w-full md:max-w-xl lg:shrink-0 xl:max-w-2xl;
-}
-.intro-text h1 {
-  @apply text-xl font-bold sm:text-5xl tracking-wider leading-8;
-  /* line-height: 3.25rem; */
-}
-
-.intro-text p {
-  @apply mt-6 text-lg sm:text-xl leading-6 md:leading-8 text-gray-800 sm:max-w-md lg:max-w-none;
-}
-</style>
